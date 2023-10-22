6c34abe09acf5c466cb520a5ea550361b57807ac
Shayn Tran <shayn.tran@sbidm.com>
2023-10-22 10:57:11 +0700

    fix branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -67,7 +67,7 @@ export const Badge = (props: Props) => {
           color: props.poweredByTextColor ?? defaultTextColor,
         }}
       >
-        <span> eFlowai</span>
+        <span> eFlowAI</span>
       </a>
     </span>
   );
